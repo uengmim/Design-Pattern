7690fa1fb74cab6677966fac8004634233531a34
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-05-09 14:07:59 +0900

    study

---
+++ src/com/company/design/Main.java
@@ -0,0 +1,20 @@
+package com.company.design;
+
+import com.company.design.singleton.Aclazz;
+import com.company.design.singleton.Bclazz;
+import com.company.design.singleton.SocketClient;
+
+public class Main {
+
+    public  static void main(String[] args) {
+
+        Aclazz aclazz = new Aclazz();
+        Bclazz bclazz = new Bclazz();
+
+        SocketClient aClient = aclazz.getSocketClient();
+        SocketClient bClient = bclazz.getSocketClient();
+
+        System.out.println("두개의 객체가 동일한가?");
+        System.out.println(aClient.equals((bClient)));
+    }
+}
---
+++ src/com/company/design/singleton/Bclazz.java
@@ -0,0 +1,14 @@
+package com.company.design.singleton;
+
+public class Bclazz {
+
+    private SocketClient socketClient;
+
+    public Bclazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+
+    public SocketClient getSocketClient(){
+        return this.socketClient;
+    }
+}
