ccf742b36c902880a40125d63d6d7034421528f2
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-05-10 10:51:11 +0900

    study

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
@@ -7,7 +8,7 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public  static void main(String[] args) {
-
+        /*
         Aclazz aclazz = new Aclazz();
         Bclazz bclazz = new Bclazz();
 
@@ -16,5 +17,21 @@ public class Main {
 
         System.out.println("두개의 객체가 동일한가?");
         System.out.println(aClient.equals((bClient)));
+        */
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+    //콘센트
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
