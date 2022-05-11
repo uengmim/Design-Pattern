5965a7b05864e0ecfa0d87d3d4913596b988b871
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-05-11 17:30:42 +0900

    study

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -3,6 +3,10 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.facade.Ftp;
+import com.company.design.facade.Reader;
+import com.company.design.facade.SftpClient;
+import com.company.design.facade.Writer;
 import com.company.design.observer.Button;
 import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.Browser;
@@ -13,6 +17,7 @@ import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
 
+import java.sql.Wrapper;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -103,7 +108,28 @@ public class Main {
         button.click(("메시지 전달 : click 3"));
         button.click(("메시지 전달 : click 4"));
         */
+        /* facade pattern
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
 
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisConnect();
+        ftpClient.disConnect();
+
+        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disConnect();
+         */
     }
     //콘센트
     public static void connect(Electronic110V electronic110V){
